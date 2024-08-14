a80cb1985aac37f3923de4d81d994836008d3391
Banushree1911 <banushreebanushree@gmail.com>
2024-08-15 00:01:22 +0530

    Add files via upload

---
+++ const.js
@@ -0,0 +1,20 @@
+
+const num = 5;
+
+console.log(num) 
+
+const x = 22;
+{
+    const x = 90;
+    console.log(x);
+
+    {
+        const x = 77;
+        console.log(x);
+    }
+    {
+        const x = 45;
+        console.log(x);
+    }
+}
+console.log(x);
