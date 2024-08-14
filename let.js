733e8e22b10df8428f558e70575bc971fccb7678
Banushree1911 <banushreebanushree@gmail.com>
2024-08-15 00:02:02 +0530

    Add files via upload

---
+++ let.js
@@ -0,0 +1,22 @@
+let num1 = 5;
+let num2 = "5";
+
+console.log(num1 !== num2); 
+
+
+
+let age = 25;
+let result = age >= 18 ? "You are an adult" : "You are not an adult";
+
+console.log(result);
+
+
+
+let x = 77;
+
+{
+    let x = 23;
+    console.log(x);
+}
+
+console.log(x);
