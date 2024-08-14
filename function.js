5871ceb075ebf7c7df1727d3ee8317c813f82262
Banushree1911 <banushreebanushree@gmail.com>
2024-08-15 00:01:42 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,15 @@
+
+function calcAddition(number1, number2) { 
+    return number1 + number2; 
+}
+console.log(calcAddition(6,9));
+
+
+function welcomeMsg(name) {
+    return ("Hello " + name + " Its my plessure to work here!");
+
+}
+
+let nameVal = "Admin";
+
+console.log(welcomeMsg(nameVal));
