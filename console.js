7a5ba8c851edfd1eb0f7ab28dae1d4d7308ba2da
Banushree1911 <banushreebanushree@gmail.com>
2024-08-15 00:01:02 +0530

    Add files via upload

---
+++ console.js
@@ -0,0 +1,8 @@
+var name = "Banu";
+var age = 20;
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.error("This is an error message.");
+console.warn("This is a warning message.");
+console.info("This is an informational message.");
