d098ee971fc3d444f4bf69de2e19300c1c8a5517
Banushree1911 <banushreebanushree@gmail.com>
2024-08-14 23:55:18 +0530

    Add files via upload

---
+++ arrow_function.js
@@ -0,0 +1,20 @@
+
+
+value = (a, b) => a * b;
+console.log(value(3, 5));
+
+number = (a, b) => {
+    c = 5;
+    return (a + b) * c;
+};
+console.log(number(2, 3));
+
+const string = () => "Technology Development";
+console.log(string);
+
+
+let array = ["dhivya", "banu", "ekakkiya"];
+ 
+let lengths = array.map((string) => string.length);
+ 
+console.log(lengths);
